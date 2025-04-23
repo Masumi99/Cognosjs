332c1bdb06907f2eb40bed132d026bba2cad40a5
Masumi99 <94692722+Masumi99@users.noreply.github.com>
2025-04-23 11:47:37 +0200

    test

--- test/Info.js
+++ test/Info.js
@@ -10,6 +10,15 @@ define
 		const containers = document.querySelectorAll('.clsCboContainer');
 		if (containers)
 		{	console.log('** containers:', containers);
+			containers.forEach(cont => 
+			{	const tbl = cont.querySelector('.clsCboOptions');
+				if (tbl)
+				{	
+					tbl.setAttribute('hal_tooltip', 'Bevat een van deze trefwoorden');
+
+				}
+
+			});
 
 		}
     }
