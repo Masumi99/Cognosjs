e54c62b89c2fe7bf2e5f18d8afeecfbcb9cbe7f7
Masumi99 <94692722+Masumi99@users.noreply.github.com>
2026-01-12 14:21:36 +0100

    Update tablepopup.js

--- tablepopup/tablepopup.js
+++ tablepopup/tablepopup.js
@@ -1,4 +1,5 @@
-define(function()
+define(['https://martinsuyl.netlify.app/tablepopup/dialog.js', 'https://martinsuyl.netlify.app/tablepopup/html.js', 'https://martinsuyl.netlify.app/tablepopup/table.js'],
+function (showDialog, constructHtml, Table) 
 {   
     function tablePopup()
     {  
@@ -12,7 +13,67 @@ define(function()
 
     tablePopup.prototype.draw = function(oControlHost)
    {  console.log('***** tablePopup');
+   
+   let doc = document.querySelector('body');
+
+	function isTableClicked(tgt)
+	{	let isTable = (tgt.nodeName === 'TABLE');
+		let parent = tgt;
+		
+		while (!isTable && parent !== doc && parent.parentElement)
+		{	parent = parent?.parentElement;
+			if (parent.nodeName === 'TABLE')
+			{	isTable = true;				
+			}
+		}
+		return {isTable, element: parent};
+	}
+	
+	function drawBlock(clientX, clientY)
+	{	const block = document.createElement('div');
+		block.style.cssText = `	width: 10px; 
+							height: 10px; 
+							background-color: red; 
+							position: absolute;
+							left: ${clientX}px;
+							top: ${clientY}px;`;
+		doc.appendChild(block); 
+	}
+
+	// dit moet hem worden: rechter muisknop ipv setPageClickEvent
+
+	function setPageClickEvent()
+	{	window.oncontextmenu = (e) => 
+		{	e.preventDefault();
+			const el = isTableClicked(e.target);
+			
+			if (el.isTable)
+			{	const table = new Table(el.element);
+				//const {str, tblStr, txtStr} = table.getSelectedTableAsString(el.element);
+				//showDialog(e, doc, str, tblStr, txtStr, table);
+		
+				//console.log('tableAsHtml', table.tableAsHtml());
+				showDialog(e, doc, table);
+			}
+		};	
+	
+	}
+
+	function test()
+	{	const tbl = document.createElement('table');
+		doc.appendChild(tbl);
+		tbl.classList.add('myTable');
+
+		tbl.innerHTML = constructHtml('table');
+
+		setPageClickEvent();
+	}
+
+	test();
+	
    }
 
    return tablePopup;
 });
+
+
