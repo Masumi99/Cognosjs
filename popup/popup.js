ad78b5f2073ca07d521a12eb86da133e2239c7c4
Masumi99 <94692722+Masumi99@users.noreply.github.com>
2026-01-16 12:53:26 +0100

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -7,13 +7,33 @@ define(function () {
 	class popup {
 		constructor() {
 		}
+
+	// -- insertStyle  --------------------------------------------------
+
+	insertStyle(oControlHost)
+	{	const cont = oControlHost.container; 
+
+		cont.innerHTML += 
+		`<style> 
+			.hidden
+			{	display: hidden;
+			}
+		</style>`;
+	}
+
+
 		// -- draw --------------------------------------------------
 		draw(oControlHost) {
-			console.log('***** Popup module draw.', `[${this.tableAttributeName}^="${this.tableName}"]`);
+			//console.log('***** Popup module draw.', `[${this.tableAttributeName}^="${this.tableName}"]`);
+			insertStyle(oControlHost);
+			this.popupBlock = document.querySelector(`[${this.tableAttributeName}^="${this.popupBlockName}"]`);
+			if (this.popupBlock)
+			{	this.popupBlock.classList.add('hidden');
+			}
 
 			const tbl = document.querySelector(`[${this.tableAttributeName}^="${this.tableName}"]`);
 			if (tbl)
-			{	console.log('**** gevonden: ', tbl);
+			{	//console.log('**** gevonden: ', tbl);
 				tbl.addEventListener('mouseover', (e) => 
 				{	if (e.target.nodeName === 'TD')
 					{	console.log('mouse over', e.target);
@@ -29,7 +49,8 @@ define(function () {
 		const o 					= oControlHost.configuration; 							// argumenten die worden meegegeven aan de custom control
 		if (o != null)																		// voorkomen dat er een foutmelding optreedt
 		{	this.tableAttributeName	= o["table attribute name"]		|| '';					// naam van attribute om naar tabel te zoeken
-			this.tableName			= o["table name"]				|| '';					// naam van de tabel
+			this.tableName			= o["tabletable name name"]		|| '';					// naam van de tabel
+			this.popupBlockName		= o["popupBlok"]				|| '';					// naam van het blok dat getoond moet worden bij popup
 		}
 
 		console.log('***** Popup module initialized.');
