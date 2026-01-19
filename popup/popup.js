10ea53d378ddca42d92a039968725893d01bd5e5
Masumi99 <94692722+Masumi99@users.noreply.github.com>
2026-01-19 12:44:42 +0100

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -4,10 +4,13 @@
 define(function () {
 	"use strict";
 
+
 	class popup 
 	{	constructor() 							// *** aan in Cognos
-		{	// oControlHost.loadingText = "Loading...";
+		{	//oControlHost.loadingText = "Loading...";
 		}
+		
+
 
 	// -- insertStyle  --------------------------------------------------
 
@@ -17,7 +20,7 @@ define(function () {
 		if (cont)
 		{	cont.innerHTML += 
 		`<style> 
-			.popupBlock_${this.uniqueId} {
+			.popupBlock {
 				position: absolute;
 				background-color: white;
 				font-family: "Segoe UI", Arial, sans-serif;
@@ -68,7 +71,7 @@ define(function () {
 
 			if (this.popupBlock)
 			{	this.popupBlock.classList.add('hidden');
-				this.popupBlock.classList.add(`popupBlock_${this.uniqueId}`);
+				this.popupBlock.classList.add('popupBlock');
 			}
 
 			const tbl = this.table = document.querySelector(`[${this.tableAttributeName}^="${this.tableName}"]`);			
@@ -112,10 +115,15 @@ define(function () {
 				this.popupBlockName		= o["popup block name"]			|| '';		// naam van het blok dat getoond moet worden bij popup
 				this.showColumns		= o["show query column names"]	|| [];		// namen van kolommen die getoond moeten worden bij popup
 				
+				this.uniqueColumnNamesTable = o["unique column names table"] || [];
+				this.uniqueColumnNamesQuery = o["unique column names query"] || [];
+				
 				console.log('oControlHost', o, this.showColumns);
 			}
 
-			this.uniqueId = oControlHost.container.getAttribute('id'); 
+			this.containerId = oControlHost.container.getAttribute('id'); 
+			console.log('*** container id', this.containerId);
+
 			this.popupBlock = document.querySelector(`[${this.tableAttributeName}^="${this.popupBlockName}"]`);
 			this.popupBlock?.classList.add('popupBlock');
 			this.X = 0;
@@ -126,16 +134,157 @@ define(function () {
 		}
 
 		setData(oControlHost, oDataStore)
-		{    this.db = oDataStore;
+		{   this.db = oDataStore;
 			this.showColumnNumbers 	= [];
+			this.uniqueColumnNumbersQuery = [];
+
 			this.showColumns.forEach(col => 
 			{	const nr = this.db.getColumnIndex(col);
 				if(nr !== NaN)
 				{	this.showColumnNumbers.push(nr);
 				}
 			});
-			console.log('showColumnNumbers', this.showColumnNumbers);
 
+			this.uniqueColumnNamesQuery.forEach(col => 
+			{	const nr = this.db.getColumnIndex(col);
+				if(nr !== NaN)
+				{	this.uniqueColumnNumbersQuery.push(nr);
+				}
+			});
+
+
+
+			console.log('uniqueColumnNumbersQuery', this.uniqueColumnNumbersQuery);
+
+		}
+	};
+
+	return popup;
+}
+);	
+
+
+
+
+
+
+// *********  1e werkende versie  *********************
+
+// Javascript module voor gebruik in Cognos
+// Versie 1: 16-1-2026 Martin Suyl
+
+define(function () {
+	"use strict";
+
+	class popup {
+		constructor() {
+		}
+
+	// -- insertStyle  --------------------------------------------------
+
+	insertStyle(oControlHost)
+	{	const cont = oControlHost.container; 
+
+		if (cont)
+		{	cont.innerHTML += 
+		`<style> 
+			.popupBlock {
+				position: absolute;
+				background-color: white;
+				border: 2px solid purple;
+				padding: 14px;
+			}
+
+			.hidden
+			{	display: none !important;
+			}
+		</style>`;
+		}
+	}
+
+	showRowContent(selectedRow)
+	{	const firstRow = this.table.querySelector('TR');
+		const tdTitles = firstRow.querySelectorAll('TD');
+		const tdValues = selectedRow.querySelectorAll('TD');
+		let str = '';
+
+		tdTitles.forEach((tr, i) => 
+		{	str += tdTitles[i].innerText + ': ' + tdValues[i].innerText + '</br>'; 
+		});
+		this.popupBlock.innerHTML = str;
+	}
+
+		// -- draw --------------------------------------------------
+		draw(oControlHost) {
+			console.log('***** Popup module draw.', `[${this.tableAttributeName}^="${this.popupBlockName}"]`);
+			this.insertStyle(oControlHost);
+			// this.popupBlock = document.querySelector(`[${this.tableAttributeName}^="${this.popupBlockName}"]`);
+			// if (this.popupBlock)
+			// {	this.popupBlock.classList.add('hidden');
+			// 	this.popupBlock.classList.add('popupBlock');
+			// }
+
+			console.log('*** Tabel', `[${this.tableAttributeName}^="${this.tableName}"]`);
+			this.table = document.querySelector(`[${this.tableAttributeName}^="${this.tableName}"]`);
+			const tbl = document.querySelector(`[${this.tableAttributeName}^="${this.tableName}"]`);
+			
+			//const panel = document.querySelector(`[hal_paneid="PinnedOnDemandToolbarPane"]`);
+			// if (panel)
+			// {	console.log('*** panel', panel, panel.style.top, panel.style.height);
+			// }
+
+			const tblRect = tbl.getBoundingClientRect();
+			if (tbl)
+			{	//console.log('**** gevonden: ', tbl);
+				const trs = tbl.querySelectorAll('TR');
+				trs.forEach(tr => 
+				{	tr.addEventListener('mouseenter', (e) => 
+					{	if (e.clientX !== this.X || e.clientY !== this.Y)
+						{	this.popupBlock.classList.remove('hidden');
+							this.popupBlock.style.top = e.clientY - tblRect.top + 20 + 'px';
+							this.popupBlock.style.left = e.clientX - tblRect.left + 20 + 'px';
+
+							this.showRowContent(tr);
+
+							this.X = e.clientX;
+							this.Y = e.clientY;
+							console.log('*** mouse enter', e);
+						}
+					});
+					// tr.addEventListener('mouseleave', (e) => 
+					// {	this.popupBlock.classList.add('hidden');
+					// 		console.log('*** mouse leave', e);
+					// });
+				});
+				// tbl.addEventListener('mouseover', (e) => 
+				// {	if (e.target.nodeName === 'TD')
+				// 	{	console.log('mouse over', e.target);
+				// 	}
+				// });
+				tbl.addEventListener('mouseleave', (e) => 
+				{	this.popupBlock.classList.add('hidden');
+					console.log('*** mouse leave', e);
+				});
+
+			}
+
+		}
+		// -- initialize --------------------------------------------------
+		initialize(oControlHost, fnDoneInitializing) {
+		const o 					= oControlHost.configuration; 							// argumenten die worden meegegeven aan de custom control
+		if (o != null)																		// voorkomen dat er een foutmelding optreedt
+		{	this.tableAttributeName	= o["table attribute name"]		|| '';					// naam van attribute om naar tabel te zoeken
+			this.tableName			= o["table name"]				|| '';					// naam van de tabel
+			this.popupBlockName		= o["popup block name"]			|| '';					// naam van het blok dat getoond moet worden bij popup
+		}
+
+		this.popupBlock = document.querySelector(`[${this.tableAttributeName}^="${this.popupBlockName}"]`);
+		this.popupBlock.classList.add('popupBlock');
+		this.X = 0;
+		this.Y = 0;
+
+		console.log('***** Popup module initialized.');
+		fnDoneInitializing();
 		}
 	};
 
